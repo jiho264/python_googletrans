e700a3c813e2cfb760777a9dece66707f3a14b9c
LEEJIHO <jiho264@naver.com>
2022-11-25 17:32:08 +0900

    init

---
+++ trans_txt.py
@@ -0,0 +1,27 @@
+# import tqdm
+from tqdm import tqdm
+# import trans module
+import googletrans
+translator = googletrans.Translator()
+# target file path
+text_file_path = './MIT_DL_01.txt'
+target = open(text_file_path, 'r')
+target_contents = target.read()
+result_file_path = 'MIT_DL_01_trans.txt'
+# result file path
+result = open(result_file_path, 'w')
+pbar = tqdm(target_contents.split("\n"))
+# RUN
+for _str in pbar:
+    # if (str is time index? == TRUE)
+    # >> SKIP trans..
+    if ((_str[1] == ":") or (_str[2] == ":")):
+        result.write(_str + "\n")
+        continue
+    _outStr = translator.translate(_str, dest='ko', src='en')
+    result.write(_outStr.text)
+    result.write("\n")
+    print(_outStr.text)
+# SAVE...
+target.close()
+result.close()
